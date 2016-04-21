b6e21694ef670238338cf240dce7bbacfd18f2e0
Nimish Agrawal <nimish.agrawal21@gmail.com>
2016-04-22 05:27:22 +0530

    Added login page and error handler

--- Python-Scripts/AOE.py
+++ Python-Scripts/AOE.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash
+from db_connect import connection
 
+#from flask.ext.mysql import MySQL
 app = Flask(__name__)
 
 
@@ -7,21 +9,54 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+
 @app.route('/index')
 def index():
     return render_template('index.html')
+
 
 @app.route('/tips')
 def tips():
     return render_template('tips.html')
 
+
 @app.route('/search')
 def search():
     return render_template('search.html')
 
+
 @app.route('/unitsearch')
 def unitsearch():
     return render_template('unitsearch.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+'''
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+    error = ''
+    try:
+        if request.method == "POST":
+            attempted_username = request.form['gTag']
+            attempted_password = request.form['password']
+            if attempted_username == "Nimish" and attempted_password == "Agrawal":
+                return redirect(url_for('index'))   #Redirects to function called index
+            else:
+                error = "Invalid credentials. Try again."
+        return render_template('login.html', error = error)
+
+    except Exception as e:
+        #flash(e)
+        return render_template('login.html', error = error)
+'''
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
 
 if __name__ == '__main__':
     app.debug = True
